0de48ae8ec0c22ad1a201b1d5d5d2c534738a9b9
Efrem Abrehe <35783634+EfremBAK@users.noreply.github.com>
2021-06-14 11:42:37 -0500

    Add files via upload

---
+++ Loops/src/Challenge1/Main.java
@@ -0,0 +1,21 @@
+package Challenge1;
+
+public class Main {
+    public static void main(String[] args) {
+        int sum = 0;
+        int count = 0;
+
+        for (int i = 1; i <= 1000; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("number is " + i);
+                count++;
+                sum = sum + i;
+            }
+
+            if (count == 5) {
+                break;
+            }
+        }
+        System.out.println("the sum of the numbers is " + sum);
+    }
+}
