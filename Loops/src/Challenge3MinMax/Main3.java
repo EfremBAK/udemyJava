88f9af34c6b17ce07e85bac202aee00276b6b92d
Efrem Abrehe <efrem.berhane2009@gmail.com>
2021-06-15 01:09:28 -0500

    min and max integer finder

---
+++ Loops/src/Challenge3MinMax/Main3.java
@@ -0,0 +1,37 @@
+package Challenge3MinMax;
+
+import java.util.Scanner;
+
+public class Main3 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+
+        while(true){
+
+            System.out.println("Enter number:");
+            boolean isAnInt = scanner.hasNextInt();
+
+            if(isAnInt){
+
+                int number = scanner.nextInt();
+                if(number>max){
+                    max = number;
+                }
+                else if(number < min){
+                    min = number;
+                }
+            }
+            else{
+                break;
+            }
+        scanner.nextLine();
+        }
+
+        System.out.println("min= " + min + ", max= " + max);
+        scanner.close();
+    }
+}
