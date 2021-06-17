0bab169fc5c7f20f4ea2f548b0a9d0852d008873
Efrem Abrehe <efrem.berhane2009@gmail.com>
2021-06-17 04:30:04 -0500

    Running car

--- Loops/src/Challenge5Inheritance/App.java
+++ Loops/src/Challenge5Inheritance/App.java
@@ -3,5 +3,11 @@ package Challenge5Inheritance;
 public class App {
     public static void main(String[] args) {
 
+
+        Tesla myTesla = new Tesla(36);
+        myTesla.accelerate(30);
+        myTesla.steer(45);
+        myTesla.accelerate(20);
+        myTesla.accelerate(-42);
     }
 }
--- Loops/src/Challenge5Inheritance/Tesla.java
+++ Loops/src/Challenge5Inheritance/Tesla.java
@@ -1,10 +1,28 @@
 package Challenge5Inheritance;
 
-public class Tesla extends Car{
+public class Tesla extends Car {
     private int roadServiceMonth;
 
     public Tesla(int roadServiceMonth) {
         super("Tesla", "Electric", 4, 2, 6, false);
         this.roadServiceMonth = roadServiceMonth;
+    }
+
+    public void accelerate(int rate) {
+        int newVelocity = getCurrentVelocity() + rate;
+        if (newVelocity == 0) {
+            stop();
+        } else if (newVelocity > 0 && newVelocity <= 10) {
+            changeGear(1);
+        } else if (newVelocity > 10 && newVelocity <= 20) {
+            changeGear(2);
+        } else if (newVelocity > 20 && newVelocity <= 30) {
+            changeGear(3);
+        } else {
+            changeGear(4);
+        }
+        if (newVelocity>0){
+            changeVelocity(newVelocity,getCurrentDirection());
+        }
     }
 }
